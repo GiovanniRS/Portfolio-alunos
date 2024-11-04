31f86694cdef053ac3c1fce98890dee36cc70f02
Giovanni Rodrigues <giovannirodrigues.2012@gmail.com>
2024-11-04 18:32:07 -0300

    Subindo componentes do portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Header from './components/Header'
+import HeroSection from './components/HeroSection'
+import AboutSection from './components/AboutSection'
+import PortfolioSection from './components/PortfolioSectiom'
+import ContactSection from './components/ContactSection'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>Giovanni Rodrigues</h1>
+    <Header />
+    <HeroSection />
+    <AboutSection />
+    <PortfolioSection />
+    <ContactSection />
   </StrictMode>,
 )
